d0db1afc98c52adf91f6b27592c0307a99263dce
fritzzuhl <fzuhl@zuhl.org>
2019-03-09 04:26:31 -0800

    changes from testing on real data cleaned code did some work-around to read-only filesystems

--- upload_to_S3.py
+++ upload_to_S3.py
@@ -11,10 +11,17 @@ logging.basicConfig(filename=my_aws_utils.filename_log(fname="logs/S3Upload_log"
                     format="%(asctime)s, Log level: %(levelname)s, msg: %(message)s",
                     level=logging.INFO)
 
+# File Source
+# LOCAL_SOURCE_DIR = '/Users/fritzzuhl/testdir/'
+LOCAL_SOURCE_DIR = '/Volumes/Seagate Backup Plus Drive/ppp/Drawings'
+
+# S3
 BUCKET_NAME = 'zuhlbucket1'
-MAJOR_DIR_ON_S3 = 'test_folder'
-DEST_DIR_ON_S3 = ''
-LOCAL_SOURCE_DIR = '/Users/fritzzuhl/testdir/'
+MAJOR_DIR_ON_S3 = 'hoosier2'
+DEST_DIR_ON_S3 = 'Drawings'
+key_prefix = MAJOR_DIR_ON_S3 + '/' + DEST_DIR_ON_S3
+
+
 file_log = 'file_log.csv'
 
 if not(os.path.exists(LOCAL_SOURCE_DIR)):
@@ -29,6 +36,7 @@ target_files= [fn for fn in my_aws_utils.get_List_Of_Local_Files(LOCAL_SOURCE_DI
               if any(fn.endswith(ext) for ext in included_extensions)]
 target_files_stripped = [i.replace(LOCAL_SOURCE_DIR,"") for i in target_files]
 target_files_stripped = [i for i in target_files_stripped if i != file_log]
+target_files_stripped = [i.strip('/') for i in target_files_stripped]
 
 file_log = LOCAL_SOURCE_DIR + file_log
 if os.path.exists(file_log):
@@ -41,22 +49,26 @@ else:
 files2consider, columns = file_log_df.shape
 
 # Connect to S3
-the_client = boto3.client('s3')
+S3_client = boto3.client('s3')
 
 # Begin Upload
 uploaded_files_count = 0
 failed_files = 0
-logging.log(logging.INFO, "******* Starting to upload %s files", len(target_files_stripped))
+total_files2upload = len(target_files_stripped)
+logging.log(logging.INFO, "******* Starting to upload %s files", total_files2upload)
 for i in range(files2consider):
     this_file, uploaded = tuple(file_log_df.loc[i])
     if uploaded:
         logging.log(logging.INFO, "Local file %s already uploaded." % this_file)
         continue
-    s3key = MAJOR_DIR_ON_S3 + '/' + this_file
+
+    s3key = key_prefix + '/' + this_file # key on S3
+    this_file_path = LOCAL_SOURCE_DIR + '/' + this_file # complete path of local file
+
     logging.log(logging.INFO, "Attempting upload: local file %s. S3 Key: %s" % (this_file, s3key))
     try:
-        my_aws_utils.upload(LOCAL_SOURCE_DIR + '/' + this_file, s3key, BUCKET_NAME, the_client)
-        logging.log(logging.INFO, "upload success")
+        my_aws_utils.upload(this_file_path, s3key, BUCKET_NAME, S3_client)
+        logging.log(logging.INFO, "upload success, %d file uploaded, %d files to go." % (i,(total_files2upload-i)))
     except NameError:
         logging.log(logging.CRITICAL, "Cannot find upload function")
     except:
@@ -73,5 +85,8 @@ else:
     logging.log(logging.INFO, "Failed to upload %d files" % failed_files)
 
 # Finally, write file_log
-file_log_df.to_csv(file_log, index=False)
+try:
+    file_log_df.to_csv(file_log, index=False)
+except OSError:
+    file_log_df.to_csv('/Users/fritzzuhl/Dropbox/pppFiler/file_logs/file_log.csv', index=False)
 
