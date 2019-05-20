4e26dc63bfe43a5f38e2827432811370aca20964
fritzzuhl <fzuhl@zuhl.org>
2019-05-20 14:28:05 -0700

    incremental

--- download.py
+++ download.py
@@ -1,21 +1,24 @@
+import sys
+sys.path.append('/Users/fritz/Dropbox/pppFiler/utils')
 import boto3
 import botocore
-import upload_S3
 import logging
-import sys
+import AWS_S3
+import local_file_handling
+import caffeine
 
 config = {
             # Change for each project
-            'S3_prefix'                 : 'hoosier1/vid-002',  # where is it in the bucket?
+            'S3_prefix'                 : 'hoosier1/vid_20190311',  # where is it in the bucket?
             # 'destination_key_prefix'    : '',   # what part of S3_prefix do you want to keep?
 
             # Rarely Changeo
             'download_log_filename_prefix': 'logs/S3download_log',
             'bucket'                    : 'zuhlbucket1',
-            'destination_dir'           : '/Users/fritzzuhl/Putain'
+            'destination_dir'           : '/Users/fritz/Putain'
           }
 
-logging.basicConfig(filename=upload_S3.filename_log(fname=config['download_log_filename_prefix']),
+logging.basicConfig(filename=local_file_handling.filename_log(fname=config['download_log_filename_prefix']),
                     filemode='w',
                     format="%(asctime)s, Log level: %(levelname)s, msg: %(message)s",
                     level=logging.INFO)
@@ -24,7 +27,7 @@ logging.basicConfig(filename=upload_S3.filename_log(fname=config['download_log_f
 # These are complete path keys that belong to a particular prefix.
 logging.log(logging.INFO, "****** Starting to download")
 try:
-    particular_keys = [x for x in upload_S3.get_key_generator(config['bucket'], prefix=config['S3_prefix'])]
+    particular_keys = [x for x in AWS_S3.get_key_generator(config['bucket'], prefix=config['S3_prefix'])]
 except KeyError:
     print("There may be a problem with the S3 parameters")
     sys.exit(1)
@@ -39,7 +42,7 @@ N = len(particular_keys)
 for i, this_key in enumerate(particular_keys):
     # add destination_dir to key_ends to create complete path
     dest_complete_path = config['destination_dir'] + '/' + this_key
-    upload_S3.make_needed_parents(dest_complete_path)
+    local_file_handling.make_needed_parents(dest_complete_path)
     try:
         s3.Bucket('zuhlbucket1').download_file(this_key, dest_complete_path)
         logging.log(logging.INFO, "Downloaded this key %s. Complete filepath %s" % (this_key,dest_complete_path))
